958c16235cf3f5c78703da4bf46326086e5708d6
Nick Mele <nickmele26@gmail.com>
2013-07-30 00:41:29 -0400

    switching to google sigin

--- config/passport.js
+++ config/passport.js
@@ -39,12 +39,10 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+// Simple route middleware to ensure user is authenticated.  Otherwise send to login page.
+exports.ensureAuthenticated = function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login')
 }
